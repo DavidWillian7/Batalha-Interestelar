e69845842b583a1c613f9be55ead5729aeca6909
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-07-23 16:26:12 -0300

    adicionando movimentacao do jogador, inimigo e tiro

--- src/game.js
+++ src/game.js
@@ -15,7 +15,31 @@ class Player extends GenericEntity{
     constructor(x,y,speed){
         super(x,y,speed);
         this.hp = 100;
+        this.points = 0;
     }
+    
+    updatePlayer(keyPressed){
+        if (keyPressed == 'ArrowUp' && player.y > 1) {
+            this.move(0,-this.speed);
+        }
+        
+        if (keyPressed == 'ArrowDown' && player.y < 400) {
+            this.move(0,this.speed);
+        }
+        
+        if (keyPressed == 'ArrowLeft' && player.x > 1) {
+            this.move(-this.speed,0);
+        }
+        
+        if (keyPressed == 'ArrowRight' && player.x < 400) {
+            this.move(this.speed,0);
+        }
+
+        if(keyPressed == ' ' && player.x < 400){
+            stage.shots.push(new Shot(player.x, player.y,25));
+        }
+    }
+
 };
 
 class Enemy extends GenericEntity{
@@ -29,13 +53,45 @@ class Stage{
         this.player = player;
         this.phase = phase;
         this.enemys = [];
+        this.shots = [];
     }
 
     setEnemys(amount){
         for(let i = 0; i < amount; i++){
-            let valor = parseInt(random(50,100));
-            this.enemys.push(new Enemy(valor,40,1));
+            let randomX = parseInt(random(0,450));
+            let randomY = parseInt(random(0,200));
+            this.enemys.push(new Enemy(randomX,-randomY,1));
         }
+    }
+
+    updateEnemys(){
+        for(let enemy of stage.enemys){
+            if(enemy.y > 500 || enemy.x > 500){
+                stage.enemys.splice(enemy,1);
+            }
+            else{
+                enemy.move(0,2);
+                rect(enemy.x,enemy.y,25,25);
+            }
+        }
+    }
+
+    updateShots(){
+        for(let shot of stage.shots){
+            if(shot.y < 0){
+                stage.shots.splice(shot,1);
+            }
+            else{
+                shot.move(0,-2);
+                rect(shot.x,shot.y,10,10);
+            }
+        } 
+    }
+}
+
+class Shot extends GenericEntity{
+    constructor(x,y,speed){
+        super(x,y,speed);
     }
 }
 
@@ -45,13 +101,23 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(500,500);
-    player = new Player(100,100,1);
+    let gameSize = 450;
+    createCanvas(gameSize,gameSize);
+    player = new Player(gameSize/2,gameSize-60,10);
     stage = new Stage(player,1);
     stage.setEnemys(4);
+
+    document.addEventListener('keydown',handleKeyDown);
+
+    function handleKeyDown(event){
+        const keyPressed = event.key;
+        player.updatePlayer(keyPressed);
+    }
 }
 
 function draw(){
     clear();
-    rect(player.x,player.y,5,5);
+    rect(stage.player.x,stage.player.y,25,25);
+    stage.updateEnemys();
+    stage.updateShots();
 }
