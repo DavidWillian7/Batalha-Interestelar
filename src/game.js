ebad9b155afc827cd3572deb44d433830515c3fe
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-07-23 21:15:45 -0300

    sistema de colisoes tiro com inimigo

--- src/game.js
+++ src/game.js
@@ -44,7 +44,7 @@ class Player extends GenericEntity{
             this.move(0,-this.speed);
         }
         
-        if (keysPressed.ArrowDown == true && player.y < 400) {
+        if (keysPressed.ArrowDown == true && player.y < 420) {
             this.move(0,this.speed);
         }
         
@@ -52,10 +52,10 @@ class Player extends GenericEntity{
             this.move(-this.speed,0);
         }
         
-        if (keysPressed.ArrowRight == true && player.x < 400) {
+        if (keysPressed.ArrowRight == true && player.x < 420) {
             this.move(this.speed,0);
         }
-        if(keysPressed.z == true && this.delayShot == false && player.x < 400){
+        if(keysPressed.z == true && this.delayShot == false){
             stage.shots.push(new Shot(player.x, player.y,25));
             this.delayShot = true;
             this.delay(5);
@@ -91,7 +91,7 @@ class Stage{
                 stage.enemys.splice(enemy,1);
             }
             else{
-                enemy.move(0,2);
+                enemy.move(0,0.7);
                 ellipse(enemy.x,enemy.y,30,30);
             }
         }
@@ -114,9 +114,9 @@ class Stage{
             for(let i = 0;i < this.enemys.length;i++){
                 for(let j = 0;j < this.shots.length;j++){
                     if(dist(this.enemys[i].x, this.enemys[i].y,this.shots[j].x,this.shots[j].y) < 20){
-                        this.enemys.splice(this.enemys[i],1);
+                        this.enemys.splice(i,1);
                         i--;
-                        this.shots.splice(this.shots[j],1);
+                        this.shots.splice(j,1);
                         j--;
                         stage.player.points += 1;
                         break;
@@ -143,7 +143,7 @@ function setup(){
     createCanvas(gameSize,gameSize);
     player = new Player(gameSize/2,gameSize-60,10);
     stage = new Stage(player,1);
-    stage.setEnemys(10);
+    stage.setEnemys(14);
     player.handleKeyboard();
 }
 
