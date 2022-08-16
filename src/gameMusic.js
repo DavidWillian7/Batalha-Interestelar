063e80953c6ed02498b5376b09f174d44f713c27
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-08-16 18:43:12 -0300

    implementando icone de musica e quando clicar colocar para tocar a musica, quando clicar novamente pausar a musica

--- src/gameMusic.js
+++ src/gameMusic.js
@@ -1,10 +1,15 @@
-let music;
+const music = new Audio('../assets/songs/gameMusic.wav');
+let sound2 = false;
 
-function preload(){
-    music = loadSound('../assets/songs/gameMusic.wav');
-}
-
-function setup(){
-    music.loop();
-    noCanvas();
+function sound(){
+    if(sound2){
+        music.pause();
+        document.getElementById("sound").src = "../assets/sound.png";
+        sound2 = false;
+    }else{
+        music.play();
+        music.loop =true;
+        document.getElementById("sound").src = "../assets/sound2.png";
+        sound2 = true;
+    }
 }
