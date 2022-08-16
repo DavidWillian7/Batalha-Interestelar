cdefeeb1fa0b6194379d8956b654b350d882b5b2
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-08-16 19:02:42 -0300

    ajustando ordem do play dentro da funcao que toca a musica

--- src/gameMusic.js
+++ src/gameMusic.js
@@ -3,13 +3,13 @@ let sound2 = false;
 
 function sound(){
     if(sound2){
-        music.pause();
         document.getElementById("sound").src = "../assets/sound.png";
         sound2 = false;
+        music.pause();
     }else{
-        music.play();
-        music.loop =true;
         document.getElementById("sound").src = "../assets/sound2.png";
         sound2 = true;
+        music.play();
+        music.loop = true;
     }
 }
