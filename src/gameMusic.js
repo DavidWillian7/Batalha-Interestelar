f755777b8b97bd877e95566be9ac6cacb62ffd15
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-08-16 22:14:07 -0300

    testando se começa tocando a musica

--- src/gameMusic.js
+++ src/gameMusic.js
@@ -1,15 +1,16 @@
 const music = new Audio('assets/songs/gameMusic.wav');
-let sound2 = false;
+let sound2 = true;
 
 function sound(){
     if(sound2){
-        document.getElementById("sound").src = "assets/sound.png";
-        sound2 = false;
-        music.pause();
-    }else{
         document.getElementById("sound").src = "assets/sound2.png";
-        sound2 = true;
+        sound2 = false;
         music.play();
         music.loop = true;
+        music.pause();
+    }else{
+        document.getElementById("sound").src = "assets/sound.png";
+        sound2 = true;
+        music.pause();
     }
 }
