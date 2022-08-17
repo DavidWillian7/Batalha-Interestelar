1d75fc024fdb133388b65cbec58696753fb0a544
DavidWillian7 <85882885+DavidWillian7@users.noreply.github.com>
2022-08-16 22:05:07 -0300

    mudando caminho das imagens e som

--- src/gameMusic.js
+++ src/gameMusic.js
@@ -1,13 +1,13 @@
-const music = new Audio('../assets/songs/gameMusic.wav');
+const music = new Audio('/assets/songs/gameMusic.wav');
 let sound2 = false;
 
 function sound(){
     if(sound2){
-        document.getElementById("sound").src = "../assets/sound.png";
+        document.getElementById("sound").src = "/assets/sound.png";
         sound2 = false;
         music.pause();
     }else{
-        document.getElementById("sound").src = "../assets/sound2.png";
+        document.getElementById("sound").src = "/assets/sound2.png";
         sound2 = true;
         music.play();
         music.loop = true;
